cda1e7a5ae8312049bf4dd10053975a08d97ea7b
ksuzue <ksuzue@protonmail.com>
2024-03-28 01:20:47 -0400

    playing around with flooding implementation, getting used to peersim; before flooding goes through entire network, now neighbors only; still buggy

--- peersim-1.0.5/src/iat/flooding/FloodingControl.java
+++ peersim-1.0.5/src/iat/flooding/FloodingControl.java
@@ -6,25 +6,56 @@ import peersim.core.Control;
 import peersim.core.Network;
 import peersim.core.Node;
 import peersim.core.CommonState;
+import peersim.edsim.EDSimulator; 
 
 public class FloodingControl implements Control {
 
+    // Constant string used as key, specifies protocol parameter
     private static final String PAR_PROTO = "protocol";
 
+    // ID of FloodingProtocol
     private final int pid;
+    private final int interval;
 
+    // Constructor
     public FloodingControl(String prefix) {
+        // No FastConfig here
         pid = Configuration.getPid(prefix + "." + PAR_PROTO);
+        // Default interval of 10 time units
+        interval = Configuration.getInt(prefix + ".interval", 10); 
     }
-
+    
+    /**
+     * Overrides Control's execute()
+     * In Cycle-Driven simulations, execute() is invoked once per cycle (WIP)
+     * In Event-Driven simulations, execute() is invoked once at the beginning
+     */
     public boolean execute() {
         // Trigger the flooding protocol from a random node
-        Node randomNode = Network.get(CommonState.r.nextInt(Network.size()));
-        int ttl = 5; // Time to live for messages
+        Node randSrc = Network.get(CommonState.r.nextInt(Network.size()));
+        // Time to live for messages
+        int ttl = 5; 
+        // Create flooding event for chosen node
+        FloodingEvent event = new FloodingEvent(randSrc, ttl);
+        // Schedule the flooding event to occur with delay intervals, deliver to randSrc
+        EDSimulator.add(10, event, randSrc, pid);
 
-        Message msg = new Message(randomNode.getIndex(), randomNode.getIndex(), "Hello from " + randomNode.getID(), ttl);
-
-        ((FloodingProtocol) randomNode.getProtocol(pid)).floodMessage(randomNode, pid, msg);
         return false;
+    }
+
+    private class FloodingEvent implements Control {
+        private final Node node;
+        private final int ttl;
+        public FloodingEvent(Node node, int ttl) {
+            this.node = node;
+            this.ttl = ttl;
+        }
+
+        public boolean execute() {
+            // Trigger flooding from the selected node
+            Message msg = new Message(node, node, "Hello from " + node.getID(), this.ttl); // Example message with TTL of 5
+            ((FloodingProtocol) node.getProtocol(pid)).floodMessage(node, pid, msg);
+            return false; // Do not request re-execution
+        }
     }
 }
--- peersim-1.0.5/src/iat/flooding/Message.java
+++ peersim-1.0.5/src/iat/flooding/Message.java
@@ -1,23 +1,25 @@
 package iat.flooding;
 
+import peersim.core.Node;
+
 public class Message {
-    private final int source;
-    private final int destination;
+    private final Node source;
+    private final Node destination;
     private final String content;
     private int ttl;
 
-    public Message(int source, int destination, String content, int ttl) {
+    public Message(Node source, Node destination, String content, int ttl) {
         this.source = source;
         this.destination = destination;
         this.content = content;
         this.ttl = ttl;
     }
 
-    public int getSource() {
+    public Node getSource() {
         return source;
     }
 
-    public int getDestination() {
+    public Node getDestination() {
         return destination;
     }
 
